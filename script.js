24acf858760d3767511edafea31469fb7ff9d93d
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2021-05-17 15:59:41 -0700

    created calculator design. Now coding the functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const allNumbers = document.querySelectorAll('[data-number]');
+const allOperands = document.querySelectorAll('[data-operations]');
+const clear = document.querySelector('[data-clear-all]');
+const clearEntry = document.querySelector('[data-clear-one]');
+const prependNumber = document.querySelector('[data-prepend]');
+const equals = document.querySelector('[data-equals]');
